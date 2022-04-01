66324b200b6a12de06592533c1927e2ea8e929c9
satyamwathrey <swathrey1@gmail.com>
2022-04-01 10:28:08 +0530

    Player Position is zero

---
+++ PlayerPositionZero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class PlayerPostionZero
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            Console.WriteLine("Player Is at Position: "+Position);
+            Console.ReadLine();
+        }
+       
+    } 
+}
