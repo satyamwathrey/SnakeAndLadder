813d48524f35f61866863b47db64dd380adf563c
satyamwathrey <swathrey1@gmail.com>
2022-04-01 10:22:13 +0530

    Welcome to the Snake and Ladder Game

---
+++ WelcomeMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class PlayerPostionZero
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder game");
+            Console.ReadLine();
+        }
+       
+    } 
+}
