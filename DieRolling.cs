addadf3bbed22e1469888e474017ad363c531f70
satyamwathrey <swathrey1@gmail.com>
2022-04-02 00:20:19 +0530

    Rolling Die using Random

---
+++ DieRolling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class PlayerPostionZero
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            int rolls = 0;
+            Console.WriteLine("Player Is at Position: "+Position);
+
+            Random random = new Random();
+            while (true)
+            {
+                rolls = random.Next(7);
+                if (rolls != 0)
+                    break;
+            }
+            Console.WriteLine("die count: " + rolls);
+            Console.ReadLine();
+        }
+       
+    } 
+}
